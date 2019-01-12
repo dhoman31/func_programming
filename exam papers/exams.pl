cc22cf18610b0233d7837df153c47688d806b0e0
= <danielhoman31@gmail.com>
2019-01-12 20:16:03 +0000

    Added my version of the 2018 Autumn.

--- exam papers/exams.pl
+++ exam papers/exams.pl
@@ -5,7 +5,14 @@ mul(List1, List2, List3) :-
   length(List3,Z),
   X*Y =:= Z.
 
-% August2018
+% August2018 - Kealan's way
 scissors(List1, C, List2, List3) :-
   append(List2, [C|List3], List),
   List = List1.
+
+% Rewrite of August 2018 - My way
+split(List1, C, List2, List3) :-
+  append(List2, [C], X),
+  append(X, List3, Y),
+  write(Y),
+  List1 = Y.
